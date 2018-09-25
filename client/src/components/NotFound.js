5dfa7944d9ad494e656cf061b4c140c5e41caeb7
kaschief <kaschiefj@gmail.com>
2018-09-25 20:15:23 +0200

    server is busy

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class NotFound extends Component {
+  componentDidMount() {
+    document.body.className = 'server';
+  }
+
+  componentWillUnmount() {
+    document.body.className = 'body';
+  }
+
   render() {
     return (
       <div className="drunk">
         <h1>404. Oops, sorry!</h1>
-        <h1> The server is having sex.</h1>
+        <h1> The server is sort of "busy".</h1>
         <br />
         <br />
         <h1> Stop looking.</h1>
